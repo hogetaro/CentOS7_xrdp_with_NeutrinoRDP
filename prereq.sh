06f11d00cf84c8abf5a2fe1138516d686eda0ca4
owner <owner@fed23a.localdomain>
2015-10-16 22:41:30 +0900

    prerequisit installer

---
+++ prereq.sh
@@ -0,0 +1,10 @@
+dnf install finger cmake patch gcc make autoconf libtool automake pkgconfig openssl-devel gettext file
+dnf install pam-devel libX11-devel libXfixes-devel libjpeg-devel
+dnf install flex bison gcc-c++ libxslt perl-libxml-perl xorg-x11-font-utils xmlto-tex
+dnf install libXrandr-devel 
+
+
+# For NeutorinoRDP
+dnf install gcc cmake openssl-devel libX11-devel libXext-devel libXinerama-devel libXcursor-devel \
+  libXi-devel libXdamage-devel libXv-devel libxkbfile-devel alsa-lib-devel cups-devel ffmpeg-devel glib2-devel
+
